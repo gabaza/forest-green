e0e2b707bdd5ff737b5a6881a7e9d6e5426c1784
Gabriela Cuello <gab.jo.cuello@gmail.com>
2019-09-16 14:49:30 -0500

    add proper links

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 function openExperience() {
     var x = document.getElementById("exp-container");
     var y = document.getElementById("image-container");
+    var z = document.getElementById("skills-container");
     if (x.style.display === "none") {
      y.style.display ="none";
+      x.style.display = "block";
+      z.style.display ="none";
+    } else {
+      x.style.display = "none";
+      y.style.display ="block";
+    
+    }
+}
+function openSkills() {
+    var x = document.getElementById("skills-container");
+    var y = document.getElementById("image-container");
+    var z = document.getElementById("exp-container");
+    if (x.style.display === "none") {
+     y.style.display ="none";
+     z.style.display ="none";
       x.style.display = "block";
     } else {
       x.style.display = "none";
